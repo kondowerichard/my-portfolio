1a3c9d407638c55559b4d8dfa40ebbebd645cf04
kondowerichard <richardkondowe8@gmail.com>
2023-05-28 23:18:23 +0200

    update

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const name = document.getElementById('name')
-const password = document.getElementById('Password')
-const form = document.getElementById('form')
+const password = document.getElementById('password')
+const inputForm = document.getElementById('inputForm')
 const errorElement = document.getElementById('error')
 
-form.addEventListener('sumbit',(e)=> {
+inputForm.addEventListener('sumbit',(e)=> {
     let message =[]
-    if(name.value === '' || name.value === null) {
-        messages.push('name is required')
+    if(name.value === '' || name.value === 'null') {
+        message.push('name is required')
     }
-    if(Password.length <= 6){
-        messages.push('Passwords must be longer than 6 characters ')
+    if(Password.value.length <= 6){
+        message.push('Passwords must be longer than 6 characters ')}
 
+     if(Password.value.length >= 20){
+            message.push('Password can not exceed 20 characters')
     }
+    if(Password.value === 'password'){
+        message.push('Password can not be sent as password')}
+
     if(message.length > 0) {
         
         e.preventDefault()
---
+++ time.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+    var greetingElement = document.getElementById('greeting');
+    var currentTime = new Date().getHours();
+  
+    if (currentTime >= 0 && currentTime < 12) {
+      greetingElement.innerHTML = "Good morning!, Rise and shine, it's a brand new day!";
+    } else if (currentTime >= 12 && currentTime < 18) {
+      greetingElement.innerHTML = "Good afternoon! Hope you're having a splendid day so far!";
+    } else {
+      greetingElement.innerHTML = "Good evening! Relax and unwind, the night is yours to enjoy!";
+    }
+  });
+  
