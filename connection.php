7205d6c96d167ac26f4b106b485325475f86f3ee
kondowerichard <richardkondowe8@gmail.com>
2023-06-09 10:27:08 +0200

    update

--- connection.php
+++ connection.php
@@ -3,11 +3,12 @@
 $localhost = "localhost";
 $username = "root";
 $password = "";
-$dbname = "login_sample_db";
+$dbname = "my-portfolio";
 $conn = mysqli_connect($localhost,$username,$password,$dbname);
 if(!$conn)
 {
-die("failed to connect!".mysqli_error());
-}else {
+die("failed to connect!".mysqli_connect_error());
+}else 
+{
 	echo" connection successful!";
 }
